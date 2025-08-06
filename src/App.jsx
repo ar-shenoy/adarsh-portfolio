26d26958b71a927b7ef4d426cc47bb4375dbc264
Adarsh <adarshrshenoy1@gmail.com>
2025-08-07 02:59:29 +0530

    portfolio v0.0

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
           playsInline
           className="fixed top-0 left-0 w-full h-full object-cover z-10 blur-md opacity-80"
         >
-          <source src="public/intro.mp4" type="video/mp4" />
+          <source src="src/assets/intro.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
